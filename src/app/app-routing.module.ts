19c04a6423c2e6d459e0b6e45430a5a2fa7f5793
Breno Miranda <brenomirandaster@gmail.com>
2021-01-01 18:09:54 -0300

    adding modules site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+// adding the layout
+
+import {SiteComponent} from './layout/site/site.component';
+
+const routes: Routes = [
+  {
+    path: '', component: SiteComponent, canActivate: [],
+    children: [
+      { path: '', loadChildren: './layout/site/site.module#SiteModule' }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { SiteComponent } from './layout/site/site.component';
 
 @NgModule({
