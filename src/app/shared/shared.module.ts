3999764eb6b879a008a8315f2ea97a851c3616d3
Breno Miranda <brenomirandaster@gmail.com>
2021-01-02 02:08:27 -0300

    create component carousel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,24 @@
 import { NgModule , NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// common
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+
+// widgets
+import { CarouselComponent } from './common/widgets/carousel/carousel.component';
 
 
 @NgModule({
   exports: [
     HeaderComponent,
     FooterComponent,
+    CarouselComponent
   ],
   declarations: [
     HeaderComponent,
     FooterComponent,
+    CarouselComponent,
   ],
   imports: [
     CommonModule,
