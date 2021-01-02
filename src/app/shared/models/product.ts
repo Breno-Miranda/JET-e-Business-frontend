d24f7f3d56a5d8a863b93f5515aa9f7eb804b0be
Breno Miranda <brenomirandaster@gmail.com>
2021-01-02 19:54:09 -0300

    create shared models class user and product

---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  id: number;
+  category: string;
+  description: string;
+  price: string;
+  discount: string;
+  stock: string;
+  url_image: string;
+  is_promotion: boolean;
+  is_activate: boolean;
+}
